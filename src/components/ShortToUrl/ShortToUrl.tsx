793f09c96b5caf1b9da2ed0a47df6f084d3ba4fd
NoFall887 <naufalamiruddin123456@gmail.com>
2022-07-03 12:30:05 +0700

    resolve shortened url

--- src/components/ShortToUrl/ShortToUrl.tsx
+++ src/components/ShortToUrl/ShortToUrl.tsx
@@ -1,20 +1,42 @@
+import axios, { AxiosResponse } from "axios";
 import React, { useState } from "react";
 import FormContainer from "../FormContainer";
 import MainBtn from "../MainBtn";
 import MainContainer from "../MainContainer";
 import TextInput from "../TextInput";
 
+interface FormSubmitResult {
+  errorMsg?: string;
+  url?: string;
+}
+
 const ShortToUrl: React.FC = () => {
-  const [url, setUrl] = useState<string>("");
+  const [url, setUrl] = useState<FormSubmitResult | null>(null);
+  const [shortenedUrl, setShortenedUrl] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  function handleSubmit(): void {}
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    setIsLoading(true);
+    e.preventDefault();
+    interface Response {
+      success: boolean;
+      message: string;
+      url: string | null;
+    }
+    axios
+      .post("checkUrl", { url: shortenedUrl })
+      .then((response: AxiosResponse<Response>) => {
+        setIsLoading(false);
+      })
+      .catch();
+  }
   return (
     <MainContainer>
       <FormContainer submitAction={handleSubmit}>
         <TextInput
           label="Enter shortened URL below!"
-          setStateAction={setUrl}
-          state={url}
+          setStateAction={setShortenedUrl}
+          state={shortenedUrl}
           disabled={isLoading}
         />
         <MainBtn submitType={true}>Check URL</MainBtn>
